cb16fa38aeffe5cf19576ca078bff49933ec422c
Alimi Alimi <alimikawualimi@yahoo.com>
2022-06-09 15:33:02 -0700

    ashar

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
-
+#Let us observe
 {
-	puts(""Programming is like building a multilingual puzzle");
+	puts(')'"Programming is like building a multilingual puzzle");
 	return 0;
 }
