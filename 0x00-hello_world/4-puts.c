373be262e3824b040412813232189612357af708
Alimi Alimi <alimikawualimi@yahoo.com>
2022-06-12 10:36:44 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,8 @@
-#include <stdio.h>
+#include <stdio>
+
 int main(void);
-/**
- * main - Entry point
- *
- * This is the document
- *
- * Return: Always 0 (Success)
- *
- */	
 
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	 printf("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
