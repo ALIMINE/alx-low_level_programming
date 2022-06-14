95a0351e739411743f7f324233a3cd0d9cc69202
Alimi Alimi <alimikawualimi@yahoo.com>
2022-06-13 19:11:03 -0700

    mssg

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,12 @@
 /* The number 5code*/
 /**
  *
- *  * main - Entry point
+ * main - Entry point
+ * Main function as advertised
+ *   
+ *   Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
