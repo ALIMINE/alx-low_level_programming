afe48fc877c43341342b9087f5e30564a37fffb4
Alimi Alimi <alimikawualimi@yahoo.com>
2022-06-13 19:06:29 -0700

    message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /* The number 5code*/
-/*This is for main*/
+/**
+ * This is for mai
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
